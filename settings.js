85ce09d2dcdecd2d5e3fbae187f6ce5cb9ce810f
radhe9828 <155523143+radhe9828@users.noreply.github.com>
2024-07-20 14:02:22 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -11,7 +11,7 @@ const FOOTERNAME = '> ＶＡＪＩＲＡ -  ＭＤ - Ｖ4';
 global.owner = process.env.OWNER_NUMBER  || '918955898763' ;  // ADD YOUR NUMBER WITHOUT +
     
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=iEMikIRY#gCscgO_tGZHnrP5uQYTnjjut6lhNEYwcJskJvvT_Txw' ,
+SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=CBcXDZjJ#0tjnUiEEuMKg2HK-qYozdrhrnwan6L7HMbhPmdQJSKE' ,
 travaSend: process.env.TRAVA_SEND || '25' ,   
 MODERATORS : process.env.MODERATORS === undefined ? "94772801923,94787820101" : process.env.MODERATORS ,    
 MAX_SIZE: process.env.MAX_SIZE === undefined ? '1536': process.env.MAX_SIZE,/*add this in megabytes*/    
